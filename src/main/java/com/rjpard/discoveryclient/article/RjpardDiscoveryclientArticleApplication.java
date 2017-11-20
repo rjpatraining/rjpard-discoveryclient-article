436dc100a1ee526b72549c265a78356b0f7ed2eb
rajmohanparayil <rjpatech@gmail.com>
2017-11-20 06:44:24 -0600

    First Commit

---
+++ src/main/java/com/rjpard/discoveryclient/article/RjpardDiscoveryclientArticleApplication.java
@@ -0,0 +1,14 @@
+package com.rjpard.discoveryclient.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RjpardDiscoveryclientArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RjpardDiscoveryclientArticleApplication.class, args);
+	}
+}
